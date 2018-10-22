c95aa0ccea1a8861c51ae0e282f5fa44bf74338c
alegrand42 <alegrand@student.42.fr>
2018-10-22 16:18:06 +0200

    missing picture added

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,6 +2,6 @@ class Character < ApplicationRecord
     has_many :inventories
     has_many :weapons, through: :inventories
     include SharedSearchMethods
-    has_attached_file :avatar, styles: { thumb: "120x120>" }, default_url: "/images/:style/missing.png"
-    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+    has_attached_file :avatar, styles: { thumb: "120x120>" }, default_url: "/images/:style/missing.jpg"
+    validates_attachment_content_type :avatar, content_type: ["image/jpeg", "image/gif", "image/png"], size: { in: 0..10.kilobytes }
 end
